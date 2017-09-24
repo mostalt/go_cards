4e30d0b70152930197b0a9d151d59baf395e5fea
Sergey Antonov <mostalt.mail@gmail.com>
2017-09-24 11:55:50 +0700

    Joinig a slice of strings

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	cards := newDeck()
 
-	hand, remaining := deal(cards, 5)
+	// hand, remaining := deal(cards, 5)
 
-	hand.print()
-	remaining.print()
+	// hand.print()
+	// remaining.print()
+
+	fmt.Println(cards.toString())
 }
